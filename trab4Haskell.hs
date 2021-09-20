620d3f9d263d9a2f7664bf597c39dcbb708a0b16
Raphael Kazuhiro Yoshiki <74300967+RaphaelYoshiki@users.noreply.github.com>
2021-09-20 18:54:53 -0300

    Update trab4Haskell.hs

--- trab4Haskell.hs
+++ trab4Haskell.hs
@@ -1,3 +1,5 @@
+import Data.Maybe
+
 type ProdutoEstoque = ([Char], Float, Int)
 type EstoqueProdutos = [([Char], Float, Int)]
 
@@ -6,8 +8,8 @@ type CarrinhoCompra = [([Char], Int)]
 
 
 --Coisas do estoque
-adicionaProduto :: ([Char], Float, Int) -> [([Char], Float, Int)]
-adicionaProduto produto = produto:[]
+adicionaProduto :: ([Char], Float, Int) -> [([Char], Float, Int)] -> [([Char], Float, Int)]
+adicionaProduto produto estoqueAtual = produto:estoqueAtual
 
 monE :: ProdutoEstoque
 monE = ("monitor", 500, 100)
@@ -21,8 +23,11 @@ tecE = ("teclado", 70, 50)
 mouE :: ProdutoEstoque
 mouE = ("mouse", 50, 50)
 
+cpuE :: ProdutoEstoque
+cpuE = ("cpu", 10050, 50)
+
 estoque :: EstoqueProdutos
-estoque = adicionaProduto monE ++ adicionaProduto telE ++ adicionaProduto tecE ++ adicionaProduto mouE
+estoque = adicionaProduto monE $ adicionaProduto telE $ adicionaProduto tecE $ adicionaProduto mouE $ adicionaProduto cpuE []
 
 
 --Coisas do carrinho
@@ -38,11 +43,19 @@ telC = ("telefone", 5)
 tecC :: ProdutoCarrinho
 tecC = ("teclado", 2)
 
+cpuC :: ProdutoCarrinho
+cpuC = ("cpu", 1)
+
 carrinho :: CarrinhoCompra
-carrinho = adicionaItem monC ++ adicionaItem telC ++ adicionaItem tecC
+carrinho = adicionaItem monC ++ adicionaItem telC ++ adicionaItem tecC ++ adicionaItem cpuC
 
 
 --Fim de compra
+get1st :: ([Char], Float, Int) -> [Char]
+get1st(x, _, _) = x
+
+get2nd :: ([Char], Float, Int) -> Float
+get2nd(_, x, _) = x
 
 get3rd :: ([Char], Float, Int) -> Int
 get3rd(_, _, x) = x
@@ -56,13 +69,42 @@ f2 = ("telefone", 150 , get3rd telE - snd telC)
 f3 :: ProdutoEstoque
 f3 = ("teclado", 70, get3rd tecE - snd tecC)
 
+f4 :: ProdutoEstoque
+f4 = ("cpu", 10050, get3rd cpuE - snd cpuC)
+
 estoqueFinal :: EstoqueProdutos
-estoqueFinal = adicionaProduto f1 ++ adicionaProduto f2 ++ adicionaProduto f3 ++ adicionaProduto mouE
-
-total :: Int
-total = snd monC * 500 + snd telC * 150 + snd tecC * 70
+estoqueFinal = adicionaProduto f1 $ adicionaProduto f2 $ adicionaProduto f3 $ adicionaProduto f4 $ adicionaProduto mouE estoque
 
 
+--Cálculo do total
+--total :: Float
+--total = (fromIntegral (snd monC)) * get2nd monE + (fromIntegral (snd telC)) * get2nd telE + (fromIntegral (snd tecC)) * get2nd tecE
+getQtd :: ([Char], Int) -> Float
+getQtd (_, x) = fromIntegral x
+
+minhalookup :: Eq a => a -> [(a,b)] -> Maybe b
+minhalookup _ [] = Nothing
+minhalookup aux1 ((val1,val2):aux2)
+    |aux1 == val1 = Just val2
+    |otherwise = minhalookup aux1 aux2
+
+getPrice :: ([Char], Float, Int) -> ([Char], Float)
+getPrice produto = (get1st produto, get2nd produto)
+    
+listPrice :: [([Char], Float)]
+listPrice = map getPrice estoque
+
+calcMult :: ([Char], Int) -> Float
+calcMult prod = (getQtd prod) * fromJust (minhalookup (fst prod) listPrice)
+
+listMult :: [Float]
+listMult = map calcMult carrinho
+
+sumTotal :: Float
+sumTotal = sum listMult
+
+
+--Main
 main :: IO ()
 main = do
 
@@ -71,4 +113,4 @@ print(estoqueFinal)
 putStrLn "Carrinho: "
 print(carrinho)
 putStrLn "Total: "
-print(total)
+print(sumTotal)
